ba7170b5dff35dd348928746ac4ca8068229578c
Rustam <rustamaaaa@bk.ru>
2021-04-07 17:21:55 +0300

    fix/исправленны ошибки

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,6 @@
+import Card from './Card.js';
+import {createCardVadidation, editProfileVadidation} from './FormValidator.js';
+
 const initialCards = [
   {
     name: 'Архыз',
@@ -20,19 +23,30 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Коломна',
+    link: 'https://images.unsplash.com/photo-1574580514971-662e3ef49eff?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80'
   }
 ];
 
-const validationConfig = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__form-input-text',
-  submitButtonSelector: '.button-popup',
-  inactiveButtonClass: 'button-popup_inactive',
-  inputErrorClass: 'popup__form-input-text_type_error',
-  errorClass: 'popup__error-message_active'
-}
+/* Возможно понадобиться в будущем */
+// const infoPopup = {
+//   namePopup: '.popup_review_img',
+//   showPopup: 'popup_opened',
+//   buttonClose: 'popup__btn',
+//   popupImg: '.popup__img',
+//   popupImgText: '.popup__img-text',
+// };
+
+/* Этот обьект для каждого шаблона свой */
+const infoTemplatePlace = {
+  name: '.place-list-template',
+  cardItem: '.place__item',
+  cardTitle: '.card-place__title',
+  cardImg: '.card-place__img',
+  buttonDeleteCard: 'card-place__delete-btn',
+  buttonLikeCard: 'card-place__like-btn',
+  buttonLikeCardActive: 'card-place__like-btn_active',
+};
 
 const popupConfig = {
   openPopup: '.popup_opened',
@@ -54,7 +68,7 @@ const popupItems = document.querySelectorAll('.popup');
 // Попап редактирования профиля
 const popupEditProfile = document.querySelector('.popup_edit_profile');
 
-// Попад добавления карточек
+// Попап добавления карточек
 const popupAddCard = document.querySelector('.popup_add_card');
 
 /* Попап просмотр картинки */
@@ -76,18 +90,17 @@ const formAddCard = popupAddCard.querySelector('.popup__form');
 const inputAddName = formAddCard.querySelector('.popup__form-input-text_value_name');
 const inputAddSrcImg = formAddCard.querySelector('.popup__form-input-text_value_does');
 
-/* Блок с сообщением об ошибке в попапе */
-const blockMessageError = document.querySelectorAll('.popup__error-message');
-/* Все поля в форме */
-const allInputPopup = document.querySelectorAll('.popup__form-input-text');
-
 /* Список карточек с местами */
 const place = document.querySelector('.place');
 const placeList = place.querySelector('.place__list');
 
-/* Шаблон карточек с местами */
-const placeListTemplate = document.querySelector('.place-list-template');
+export function handleCardClick(name, link) {
+  imgPopup.src = link;
+  imgPopup.alt = name;
+  textPopupShowImg.textContent = name;
 
+  addPopup(popupShowImg);
+}
 
 const listenerPopupKey = event => {
   if (event.key === 'Escape') {
@@ -109,24 +122,8 @@ function removePopup(popupItem) {
   document.removeEventListener('keydown', listenerPopupKey);
 }
 
-/* Скрывает кнопку при очистке полей. Т.к поля очищаются, нужно при этом скрывать кнопку */
-/* Функция манипулирует с кнопкой у попапа, относиться к валидации, но переноситься не может */
-const hideButtonForm = form => {
-  const buttonFormSubmit = form.querySelector(validationConfig.submitButtonSelector);
-  buttonFormSubmit.classList.add(validationConfig.inactiveButtonClass);
-  buttonFormSubmit.disabled = true;
-}
-
-/* Показывает кнопку при добавлении в форму текста. Т.к текст в полях уже есть, кнопку надо показывать */
-const showButtonForm = form => {
-  const buttonFormSubmit = form.querySelector(validationConfig.submitButtonSelector);
-  buttonFormSubmit.classList.remove(validationConfig.inactiveButtonClass);
-  buttonFormSubmit.disabled = false;
-}
-
 function clearFormInput(form) {
   form.reset();
-  hideButtonForm(form);
 }
 
 
@@ -140,32 +137,12 @@ function editProfile() {
 function editInputValue() {
   inputEditName.value = titleName.textContent;
   inputEditDoes.value = subtitleDoes.textContent;
-
-  /* При добавлении в поля текста, кнопку при этом нужно показывать */
-  showButtonForm(formEditProfile);
-}
-
-/* Очищает текст в с ошибкой в попапе */
-const clearTextFormMessage = () => {
-  blockMessageError.forEach(block => {
-    block.textContent = '';
-    block.classList.remove(validationConfig.errorClass);
-  });
-}
-
-/* Удаляет класс со всех инпутов */
-const clearClassInputs = () => {
-  allInputPopup.forEach(input => {
-    input.classList.remove(validationConfig.inputErrorClass);
-  });
 }
 
 /* Открытие попапа редактирования профиля */
 function openPopupEditProfile() {
-  clearTextFormMessage();
-  clearClassInputs();
-
   editInputValue();
+  editProfileVadidation.resetValidation();
   addPopup(popupEditProfile);
 }
 
@@ -201,27 +178,33 @@ function formSubmitHandlerEditProfile (event) {
 
 formEditProfile.addEventListener('submit', formSubmitHandlerEditProfile);
 
+/* При загрузке страницы данные в карточки заносятся из массива */
+showCards(initialCards, placeList);
+
+/* Клонирует элемент шаблона, и выводит в карточку с местами, данные из массива */
+function showCards(arrCardList, container) {
+  arrCardList.forEach(item => {
+    /* В переменную присваивается возвращаемое значение функции */
+    const placeItem = createCard(item.name, item.link);
+
+    /* Добавляется узел в верстку */
+    container.append(placeItem);
+  });
+}
+
 /* Создаст одну карточку с местами. Принимает в параметры имя карточки, и ссылку на картинку */
-function createCard(nameCard, linkImg) {
-  const placeItem = placeListTemplate.content.querySelector('.place__item').cloneNode('true');
+function createCard(name, link) {
+  const initialCards = { name, link };
 
-  placeItem.querySelector('.card-place__title').textContent = nameCard;
+  const cardPlace = new Card(initialCards, infoTemplatePlace, handleCardClick);
 
-  const imgNode = placeItem.querySelector('.card-place__img');
-
-  imgNode.src = linkImg;
-  imgNode.alt = nameCard;
-
-  /* Добавил события. Все события вешаются до того как DOM узлы будут добавлены в верстку. События вешаются при создании DOM узлов */
-  eventsAddEl(placeItem);
-
-  return placeItem;
+  return cardPlace.fillCardTemplate();
 }
 
 /* Открытие попапа создания карточки */
 function openPopupAddCard() {
-  clearTextFormMessage();
-  clearClassInputs();
+  createCardVadidation.resetValidation();
+
   addPopup(popupAddCard);
 }
 
@@ -237,47 +220,11 @@ function formSubmitHandlerAddCard (event) {
   /* В переменную присваивается возвращаемое значение функции. Создаст одну карточку */
   const placeItem = createCard(inputAddNameValue, inputAddSrcImgValue);
 
-  /* Добавил события. Все события вешаются до того как DOM узлы будут добавлены в верстку */
-  eventsAddEl(placeItem);
-
   placeList.prepend(placeItem);
 
-  clearFormInput(formAddCard);
-
   removePopup(popupAddCard);
+
+  clearFormInput(formAddCard);
 }
 
 formAddCard.addEventListener('submit', formSubmitHandlerAddCard);
-
-/* Меняет класс в кнопке */
-function likeCard(event) {
-  event.target.classList.toggle('card-place__like-btn_active');
-}
-
-/* Удаляет карточку с верстки и с массива */
-function deleteCard(event) {
-  event.target.closest('.place__item').remove();
-}
-
-/* Попап просмотр картинки */
-function showImg(event) {
-  imgPopup.src = event.target.src;
-
-  const titleCard = event.target.nextElementSibling.textContent;
-  textPopupShowImg.textContent = titleCard;
-
-  imgPopup.alt = titleCard;
-
-  addPopup(popupShowImg);
-}
-
-/* Вызывается до добавления DOM узла в верстку. Вызывает у найденных элементов события. Принимает элемент на который вешаются эти события */
-function eventsAddEl(item) {
-  /* Все события вешаются до того как DOM узлы будут добавлены в верстку */
-  /* Удаляется карточка */
-  item.querySelector('.card-place__delete-btn').addEventListener('click', deleteCard);
-  /* Ставиться лайк */
-  item.querySelector('.card-place__like-btn').addEventListener('click', likeCard);
-  /* Просмотр картинок из карточки в модальном окне */
-  item.querySelector('.card-place__img').addEventListener('click', showImg);
-}
